739e89a0590c88841ae7e42d5de5fd19bc69eeb8
Roeut Chak <roeut.chak@gmail.com>
2015-07-27 15:48:08 -0400

    Initial Commit

---
+++ TipCalculatorRoeutUITests/TipCalculatorRoeutUITests.swift
@@ -0,0 +1,34 @@
+//
+//  TipCalculatorRoeutUITests.swift
+//  TipCalculatorRoeutUITests
+//
+//  Created by Roeut on 7/27/15.
+//  Copyright © 2015 NaturesRenegade. All rights reserved.
+//
+
+import XCTest
+
+class TipCalculatorRoeutUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
